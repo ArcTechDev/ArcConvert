c0df95c9ef986820d5cf8df5c31dc996e71c48c3
Nelson <tomneo2004@gmail.com>
2016-04-26 11:35:33 +0800

    2016/4/26 -Fix banner for history -Prepare for 3D Touch

--- Calculator/AppDelegate.m
+++ Calculator/AppDelegate.m
@@ -82,4 +82,8 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (void)application:(UIApplication *)application performActionForShortcutItem:(UIApplicationShortcutItem *)shortcutItem completionHandler:(void (^)(BOOL))completionHandler{
+    
+}
+
 @end
